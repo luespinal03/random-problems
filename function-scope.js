3e9a8ecadfc60a8a471e3c3d50d85b8b02b48ae2
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-06-13 10:03:26 -0400

    function.js

---
+++ function-scope.js
@@ -0,0 +1,20 @@
+
+// Global (convertFahrenheitToCelsius, tempOne, tempTwo)
+  // Local scope (fahrenheit, celsius)
+     // Local scope (isFreezing)
+
+let convertFahrenheitToCelsius = function (fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9
+
+    if (celsius <= 0) {
+        let isFreezing = true;
+    }
+
+    return celcius
+}
+
+let tempOne = convertFahrenheitToCelsius(32)
+let tempTwo = convertFahrenheitToCelsius(68)
+
+console.log(tempOne)
+console.log(tempTwo)
