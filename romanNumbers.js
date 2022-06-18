3ecc6c548a89799a654f843a4dfcaa17dd881b71
Luis Espinal <luisespinal294@gmail.com>
2022-06-18 12:45:48 -0400

    updated romanNumbers.js

--- romanNumbers.js
+++ romanNumbers.js
@@ -8,8 +8,6 @@ romanNums = {
     M: 1000
 }
 
-let s = "DCXXI" // 1994 
-//      50, 5, 3  
 
 function romanToInt(s) {
     let tab = 0;
@@ -17,29 +15,30 @@ function romanToInt(s) {
 
     for (let i = 0; i < s.length; i++) {
 
-        if (s[i] === "I" && s[i + 1] === "V") {
-            tab += 4;
-            i++;
-        } else if (s[i] === "I" && s[i + 1] === "X") {
-            tab += 9;
-            i++;
-        } else if (s[i] === "X" && s[i + 1] === "L") {
-            tab += 40;
-            i++;
-        } else if (s[i] === "X" && s[i + 1] === "C") {
-            tab += 90;
-            i++;
-        } else if (s[i] === "C" && s[i + 1] === "D") {
-            tab += 400;
-            i++;
-        } else if (s[i] === "C" && s[i + 1] === "M") {
-            tab += 900;
-            i++;
+        if (s[i] === "I" && s[i + 1] === "V") { // 5 - 1 
+            tab += 4;   //next index to the right
+            i++; // moves on to the next index
+        } else if (s[i] === "I" && s[i + 1] === "X") { // 10 - 1
+            tab += 9;  //next index to the right
+            i++; // moves on to the next index
+        } else if (s[i] === "X" && s[i + 1] === "L") { // 50 - 10
+            tab += 40;  //next index to the right
+            i++; // moves on to the next index
+        } else if (s[i] === "X" && s[i + 1] === "C") { // 100 - 10
+            tab += 90;  //next index to the right
+            i++; // moves on to the next index
+        } else if (s[i] === "C" && s[i + 1] === "D") { // 500 - 100
+            tab += 400;  //next index to the right
+            i++; // moves on to the next index
+        } else if (s[i] === "C" && s[i + 1] === "M") { // 1000 - 100
+            tab += 900;  //next index to the right
+            i++; // moves on to the next index
         } else {
-            tab += romanNums[s[i]];
+            tab += romanNums[s[i]];  // if nothing hits, log the number into tab
         }
     }
     return tab;
 }
 
+let s = "DCXXI"
 console.log(romanToInt(s))
