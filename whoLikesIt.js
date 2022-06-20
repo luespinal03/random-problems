2de60b146357513f8cd793aa54c84ca32fb3e403
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-06-20 12:46:45 -0400

    whoLikesIt.js

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -7,7 +7,7 @@ function likes(names) {
         return `${names[0]} and ${names[1]} like this`
     } else if (names.length === 3) {
         return `${names[0]}, ${names[1]} and ${names[2]} like this`
-    } else if (names.length === 4) {
+    } else if (names.length > 4) {
         return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
     }
 }
