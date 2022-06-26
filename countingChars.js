23b06fe1fd7003bbe4d8e85fb0f04055349ca86a
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-06-26 12:56:57 -0400

    countingChars.js

---
+++ countingChars.js
@@ -0,0 +1,18 @@
+function count(string) {
+    let newStr = string.split("");
+    let obj = {};
+
+    for (let i = 0; i < newStr.length; i++) {
+        if (typeof obj[newStr[i]] !== 'undefined') {
+            // if newStr[i] is not in obj then it goes into obj and add 1 as the value
+            obj[newStr[i]] += 1;
+        } else {
+            // if it is in there already add a value of 1 to w.e number it has already 
+            obj[newStr[i]] = 1;
+
+        }
+    }
+    return obj;
+}
+
+console.log(count('aba'));
