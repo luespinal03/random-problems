170a6bba6bdd313203f9a076d218134d1d9ad45a
Luis Espinal <luisespinal294@gmail.com>
2022-06-26 12:58:43 -0400

    updated

--- countingChars.js
+++ countingChars.js
@@ -1,11 +1,15 @@
 function count(string) {
     let newStr = string.split("");
     let obj = {};
+
     for (let i = 0; i < newStr.length; i++) {
         if (typeof obj[newStr[i]] !== 'undefined') {
+            // if newStr[i] is not in obj then it goes into obj and add 1 as the value
             obj[newStr[i]] += 1;
         } else {
+            // if it is in there already add  1 to w.e the value it has already 
             obj[newStr[i]] = 1;
+
         }
     }
     return obj;
