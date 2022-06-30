661d54a745a45d136df2e3efba5982d476eb6359
Luis Espinal <luisespinal294@gmail.com>
2022-06-29 22:08:08 -0400

    addString.js

--- addString.js
+++ addString.js
@@ -7,18 +7,28 @@
 // addStringIfLastS(['you', 'should', 'not', 'see'], 'TEST')) //['you', 'should', 'not', 'see']
 
 function addStringIfLastS(arrays, sillyStrings) {
+    let newArray = []
     for (let i = 0; i < arrays.length; i++) {
         // takes the value from every [i]
-        let arrayString = arrays[i] 
+        let arrayString = arrays[i]
+        
         // if the last index of our indexes equals 's' || 'S'
-        if(arrayString[arrayString.length -1] === 's'|| arrayString[arrayString.length -1] === 'S'){
+        if (arrayString[arrayString.length - 1] === 's' || arrayString[arrayString.length - 1] === 'S') {
             // if we find an array[i] that has the last index be a 's' || 'S'
-            
-        /* then that same array[i] = array[i].concat(sillyStrings) */ arrays[i] = arrays[i].concat(sillyStrings) 
+
+            /* then that same array[i] = array[i].concat(sillyStrings) */
+            // arrays[i] = arrays[i].concat(sillyStrings);
+            newArray.push(arrays[i].concat(sillyStrings));
+        } else {
+            newArray.push(arrays[i])
         }
     }
-    return arrays
+    // return arrays
+    return newArray
 }
 
 console.log(addStringIfLastS(['test', 'testing', 'testings'], 'TEST')); //  => ['test', 'testing', 'testingsTEST']
 console.log(addStringIfLastS(['you', 'should', 'not', 'see', ], 'TEST')); //['you', 'should', 'not', 'see']
+
+
+// ========================[ Empty Array Method ]===============================
