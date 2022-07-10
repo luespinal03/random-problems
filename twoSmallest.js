70f1a5966ef9e1e0d6e2cb2274bf315891f32d2d
Luis Espinal <luisespinal294@gmail.com>
2022-07-10 11:04:00 -0400

    Add files via upload

--- twoSmallest.js
+++ twoSmallest.js
@@ -1,8 +1,10 @@
 function twoSmallest(banana) {
     let smallestNumber = 100;
     let largestNumber = 0;
-    let secondSmallestNumber = smallestNumber;
-    let restOfArray = []
+    // let secondSmallestNumber = smallestNumber;
+    let restOfArray = [];
+    let largestFromRestOfArray = 0;
+    let smallestFromRestOfArray = 100;
     let answers = [];
 
     if (banana.length < 2) {
@@ -19,7 +21,7 @@ function twoSmallest(banana) {
     }
 
     // finds the largest number
-    for(let i = 0; i < banana.length; i++){
+    for (let i = 0; i < banana.length; i++) {
         if (banana[i] > largestNumber) {
             largestNumber = banana[i];
         } else {
@@ -29,18 +31,21 @@ function twoSmallest(banana) {
 
 
     // finds the middle number
-    for(let i = 0; i < restOfArray.length; i++){
-        if(restOfArray[i] === largestNumber && restOfArray[i] === smallestNumber){
-            continue; 
-        } else if(restOfArray[i] < largestNumber && restOfArray[i] > smallestNumber){
-            secondSmallestNumber = (restOfArray[i]);
-        }
+    for (let i = 0; i < restOfArray.length; i++) {
+        if (restOfArray[i] === smallestNumber || restOfArray[i] === largestNumber) {
+            continue;
+
+        } else if (restOfArray[i] > largestFromRestOfArray) {
+            largestFromRestOfArray = restOfArray[i];
+
+        } else if(restOfArray[i] < smallestFromRestOfArray) 
+            smallestFromRestOfArray = restOfArray[i];
     }
+
     answers.push(smallestNumber)
-    answers.push(secondSmallestNumber);
+    answers.push(smallestFromRestOfArray);
     return answers;
 
 }
 
 console.log(twoSmallest([10, 2, 6, 7, 5])) // 2,5
-
